aa8a3b303d4ede8d1a9ab3b130e026c156ee89ae
Nohelia Ludeña <nohealejandra7@gmail.com>
2019-04-25 11:35:37 -0500

    ejerciciosSeleccionSimple

---
+++ Ejercicios/src/SeleccionSimple.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+import paquetedos.Operacion;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nali1
+ */
+public class SeleccionSimple {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+         Scanner entrada = new Scanner(System.in);
+        
+        // método principal
+        
+        String miMensaje = Operacion.mensaje;
+        int calificacion;
+        int calificacion_2;
+        
+        
+        System.out.println("Ingrese la primera calificacion");
+        calificacion = entrada.nextInt();
+        
+        System.out.println("Ingrese la segunda calificación");
+        calificacion_2 = entrada.nextInt();
+        
+        if (calificacion>=85){
+            System.out.printf("%s %d\n",miMensaje, calificacion);
+            
+        }
+        
+        
+        if (calificacion_2>= 85) {
+            System.out.printf("%s %d\n",miMensaje, calificacion_2);
+        }
+    }
+    }
+    
+    
+
